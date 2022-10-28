fbad9010456de2702cf2f4634df0a433d0aa2d8c
Boris <boriszugic123@gmail.com>
2022-10-27 22:00:17 -0400

    *uploading the correct file this time* implemented printEmployees function to print all employees records in a readable format

--- employee.c
+++ employee.c
@@ -14,7 +14,19 @@ void printEmployee(PersonRec person)
 
 void printEmployees(PersonRec *person, int numRecords)
 {
+    int i;
 
+    printf("-------------------------------------\n");
+    for (i = 0 ; i < numRecords ; i++)
+    {
+        if (person[i].emplyeeOrPatient == 0)
+        {
+            char temp [33];
+            sprintf(temp, "%s %s", person[i].firstName, person[i].familyName);
+            printf("%-33s dept:%2d salary:%9.2f position:%2d years of service:%4d salary to date:%11.2f\n", temp, person[i].emp.department, person[i].emp.salary, person[i].emp.position, person[i].emp.yearsService, person[i].emp.salary*(person[i].emp.yearsService - 1));
+        }
+    }
+    printf("-------------------------------------\n\n");
 
     // add code
 
